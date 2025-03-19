4003c468cf6f0c6c6dceed03f5808ff3b0c2cf00
Hagie156 <hagieben_krishna@outlook.com>
2025-03-19 14:28:05 +0000

    Update Bank.py
    This is to provide better context for the problem

--- Problem 1/Bank.py
+++ Problem 1/Bank.py
@@ -1,12 +1,15 @@
+# This is a Seinfeld reference. The bank will give you a cerytain amount of money depending on how you greet the prompt
+
+
 greeting = input("Greetings!")
 #This next line ensures case-insensitivity is fixed
 adjusted = greeting.strip().lower()
 
 
 if "hello" == adjusted:
-    print("$0")
+    print('As you said "hello" you will receive $0')
     # For the next line, you canalso write adjusted[0] == "h"
 elif adjusted.startswith('h'):
-    print("$20")
+    print('Your greeting started with the letter "h" so you will only receive $20")
 else:
-    print("$100")
+    print('Fair play here is $100')
