ae4b6c46dcd170e137f9977038bb1f4d555bb91a
Hagie156 <hagieben_krishna@outlook.com>
2025-03-19 14:29:35 +0000

    Update Deep.py
    This is to provide better context to the problem

--- Problem 1/Deep.py
+++ Problem 1/Deep.py
@@ -1,3 +1,6 @@
+#This is a refernce to the book "a hitchhiker's guide to the galaxy"
+
+
 #This is to get the users' response
 response = input("What is the answer to the great question of life, the universe and everything?")
 #This is to ensure any mistakes or accidents will get sorted such as extra spaces or unnecessary capital letters 
