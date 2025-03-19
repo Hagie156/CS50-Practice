2361a129ca705f91d1daab88de2789f4d2cf05e4
Hagie156 <hagieben_krishna@outlook.com>
2025-03-19 14:12:14 +0000

    Create Indoor.py

---
+++ Problem 0/Indoor.py
@@ -0,0 +1,10 @@
+# Ask for input in capitals
+Capital = input("Write anything in capital letters and I will lowercase it:" )
+
+#Apply lowercase to their input
+
+Capital = Capital.lower()
+
+#Print the results with all lowercase
+
+print(f"{Capital}")
